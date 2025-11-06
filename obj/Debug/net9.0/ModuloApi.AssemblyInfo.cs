6da9fd661b6481587fcdfe5c16b43e138e631305
karolrdg <karolinerodrigues12@outlook.com>
2025-11-06 17:28:16 -0300

    feat: add class

--- obj/Debug/net9.0/ModuloApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/ModuloApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d69c818fb031486c5f30f52a247fb5b081293fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6160c98043f4f377f89374797f3befd884bba180")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
